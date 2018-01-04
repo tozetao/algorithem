d927ce603c2795234b4ee7d49ce6a247d29e79c5
zetao <tozetao@163.com>
2018-01-04 23:12:00 +0800

    1-4

--- tree/stack.php
+++ tree/stack.php
@@ -11,13 +11,11 @@ class Stack
     //数组，模拟完全二叉树
     public $array;
 
-
-
-    //初始化
-
-    //新增
-
-    //删除
+    public function __construct()
+    {
+        $this->n = 1;
+        $this->array = array();
+    }
 
     //向下比较
     public function siftdown($i)
@@ -28,20 +26,18 @@ class Stack
         //如果i节点拥有左子节点，则循环
         while($i*2<=$this->n && $flag === 0)
         {
-            //判断i节点的值是否大于左子节点
+            //父节点与俩个子节点这3个节点进行比较，寻找出最小的节点的索引，然后交换父节点与最小节点的值
             if($this->array[$i] > $this->array[$i*2])
                 $temp = $i*2;
             else
                 $temp = $i;
 
-            //判断i节点是否大于右子节点
             if($i*2+1<=$this->n)
             {
-                if($this->array[$i] > $this->array[$i*2+1])
+                if($this->array[$temp] > $this->array[$i*2+1])
                     $temp = $i*2+1;
             }
 
-            //
             if($temp != $i)
             {
                 $this->swap($i, $temp);
@@ -74,6 +70,7 @@ class Stack
         }
     }
 
+    //交换俩个数据项
     private function swap($l, $j)
     {
         $temp = $this->array[$l];
@@ -81,43 +78,85 @@ class Stack
         $this->array[$j] = $temp;
     }
 
-    //在堆首部插入一个数据
-    public function insertFirst()
-    {
-        
-    }
-
     //在堆中尾部插入一个数据项
-    public function insertLast()
+    public function insert($item)
     {
-
+        $this->array[$this->n] = $item;
+        $this->siftup($this->n);
+        $this->n++;
     }
 
     //移除并返回堆中的最小数据项
     public function remove()
     {
-        
+        $temp = $this->array[1];
+
+        $this->array[1] = $this->array[--$this->n];
+
+
+        $this->siftdown(1);
+
+        return $temp;
     }
 
-    public function create()
+    public function create($array)
     {
         //把n个元素建立一个堆
 
         //1. 依次通过siftup(i)方法插入数据项，时间复杂度为O(NlogN);
 
         //2. 从上到下从左到右依次对n个元素进行1-n的编码存储到数组中，这样可以将n个元素转换成一颗完全二叉树，从这颗完全二叉树的最后一个非叶子节点开始检查每颗子树是否满足最小堆，当所有节点的子树都满足最小堆的要求，这棵树就符合最小堆的特性，它的时间复杂度是O(N)
+
+        $this->array = $array;
+
+        for($i=$this->n/2; $i>=1; $i--)
+        {
+            $this->siftdown($i);
+        }
     }
 
     //堆排序
     //每次删除顶部元素并将其放入一个数组中，直到堆为空。
     public function sort()
     {
-        $temp = $this->array[1];
-
-        $this->array[1] = $this->array[$this->n];
-        $this->siftdown(1);
+        $sorted = array();
         $this->n--;
 
-        return $temp;
+        while($this->n > 0)
+        {
+            $temp = $this->array[1];
+            $sorted[] = $temp;
+
+            $this->array[1] = $this->array[$this->n];
+
+            $this->siftdown(1);
+            $this->n--;
+
+        }
+
+        return $sorted;
     }
 }
+
+//99 5 36 7 22 17 46 12 2 19 25 28 1 92
+$stack = new Stack();
+$stack->insert(99);
+$stack->insert(5);
+$stack->insert(36);
+$stack->insert(7);
+$stack->insert(22);
+$stack->insert(17);
+$stack->insert(46);
+$stack->insert(12);
+$stack->insert(2);
+$stack->insert(19);
+$stack->insert(25);
+$stack->insert(28);
+$stack->insert(1);
+$stack->insert(92);
+
+echo '<pre/>';
+print_r($stack->array);
+
+$sorted = $stack->sort();
+print_r($sorted);

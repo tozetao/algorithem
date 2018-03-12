6db87d31f0b04e6278f13c784cc385f0f1cbdcad
zetao <tozetao@163.com>
2018-03-12 23:47:58 +0800

    3-12

--- list/Code2_IntersectNode.php
+++ list/Code2_IntersectNode.php
@@ -1,7 +1,8 @@
 <?php
 
+include "./Code2_List.php";
+
 /*
- *
 题目：
     俩个单链表相交的一系列问题
 
@@ -14,61 +15,181 @@
  */
 class IntersectNode
 {
-    //俩个无环链表是否相交
-    public function isIntersect($link1, $link2)
+    //返回相交的节点
+    public function getIntersectNode($link1, $link2)
     {
-        $lastNode1 = null;
-        $lastNode2 = null;
+        $loop1 = $this->findLoopNode($link1);
+        $loop2 = $this->findLoopNode($link2);
 
-        while($link1!= null || $link2 != null)
-        {
-            if($link1 != null)
-            {
-                $lastNode1 = $link1;
-                $link1 = $link1->next;
-            }
+        //俩个无环链表的处理
+        if(!$loop1 && !$loop2)
+            return $this->noLoop($link1, $link2);
 
-            if($link2 != null)
-            {
-                $lastNode2 = $link2;
-                $link2 = $link2->next;
-            }
-        }
-
-        //俩个无环链表相交了
-        if($lastNode1 == $lastNode2)
-        {
-
-        }
+        //俩个有环链表的处理
+        if($loop1 != null && $loop2 != null)
+            return $this->bothLoop($link1, $link2);
 
         return null;
     }
+
+    //查询链表的入环节点
+    public function findLoopNode($link)
+    {
+        $slow = $link;
+        $fast = $link;
+
+        $flag = false;
+
+        while($fast->next != null && $fast->next->next != null)
+        {
+            $fast = $fast->next->next;
+            $slow = $slow->next;
+
+            if($fast == $slow)
+            {
+                $flag = true;
+                break;
+            }
+        }
+
+        if(!$flag) return false;
+
+        //寻找入环点
+        $fast = $link;
+        while($fast != $slow)
+        {
+            $fast = $fast->next;
+            $slow = $slow->next;
+        }
+        return $fast->data;
+    }
+    
+    //俩个无环链表的处理
+    public function noLoop($link1, $link2)
+    {
+        $length1 = 0;
+        $length2 = 0;
+
+        //计算俩个链表长度
+        $tlink1 = $link1;
+        $tlink2 = $link2;
+
+        while($tlink1 != null)
+        {
+            $length1++;
+            $tlink1 = $tlink1->next;
+        }
+
+        while($tlink2 != null)
+        {
+            $length2++;
+            $tlink2 = $tlink2->next;
+        }
+
+        $dval = abs($length1-$length2);
+        
+        if($length1 > $length2)
+        {
+            for($i = 0; $i<$dval; $i++)
+                $link1 = $link1->next;
+        }
+        else if($length1 < $length2)
+        {
+            for($i = 0; $i<$dval; $i++)
+                $link2 = $link2->next;
+        }
+
+        while($link1 != null && $link2 != null)
+        {
+            if($link1 == $link2)
+                return $link1;
+
+            $link1 = $link1->next;
+            $link2 = $link2->next;
+        }
+
+        return false;
+    }
+
+    //俩个有环链表的处理
+    public function bothLoop($link1, $link2, $loop1, $loop2)
+    {
+        //A链表指向B链表的非环节点
+        if($loop1 == $loop2)
+        {
+            $n = 0;
+            $curr1 = $link1;
+            $curr2 = $link2;
+
+            while($curr1 != $loop1)
+            {
+                $n++;
+                $curr1 = $curr1->next;
+            }
+            while($curr2 != $loop1)
+            {
+                $n--;
+                $curr2 = $curr2->next;
+            }
+
+            //判断哪个链表长
+            $curr1 = ($n > 0) ? $link1 : $link2;
+            $curr2 = ($curr1 == $link1) ? $link2: $link1;
+
+            while($n != 0)
+            {
+                $curr1 = $curr1->next;
+                $n--;
+            }
+
+            while($curr1 != $curr2)
+            {
+                $curr1 = $curr1->next;
+                $curr2 = $curr2->next;
+            }
+            return $curr1;
+        }
+        else
+        {
+            $curr = $loop1->next;
+
+            while($curr != $loop1)
+            {
+                if($curr == $loop2)
+                    return $loop1;
+
+                $curr = $curr->next;
+            }
+
+            return null;
+        }
+    }
 }
 
+$n1 = new Node(1);
+$n2 = new Node(2);
+$n3 = new Node(3);
+$n4 = new Node(4);
+$n5 = new Node(5);
+
+$n10 = new Node(10);
+$n11 = new Node(11);
+$n12 = new Node(12);
+
+$n1->next = $n2;
+$n2->next = $n3;
+$n3->next = $n4;
+$n4->next = $n5;
+$n5->next = $n4;
+
+$n10->next = $n11;
+$n11->next = null;
+
+echo '<pre/>';
+
+//俩个无环链表是否相交
 $obj = new IntersectNode();
-$ref =& $obj;
+//var_dump($intersect->isIntersect($n1, $n10));
 
-var_dump($ref);
-
-
-
-/*
-
-俩个单链表相交会有这几种情况：
-
-- 俩个无环的链表相交
-
-- 一个有环的链表和一个无环的链表相交
-    一个有环的链表和一个无环的链表，节点是不可能相交的，如果相交会破坏链表的next指针结构。
-
-- 俩个有环链表相交
-    俩个有环的链表，会有3种可能，
-    第一种是俩个有环的链表都是独立的
-    第二种是B链表的尾部节点指向A链表的环中某个节点，
-    最后是B链表的尾部节点指向A链表非环节点中的某个节点
-
-    如果A链表的入环节点等于B链表的入环节点，属于最后种情况，我们将环屏蔽掉，在求相交节点其实是求无环链表的相交节点。
-
-    如果俩个入环节点都不相等，则属于第一和第二俩种情况，这时候用A链表的入环节点往下一个节点遍历，如果能找到B链表的入环节点则属于第二种情况，
-    否则属于第一种情况，针对第二种情况，俩个入环节点都可以算作相交的点，只不过距离不同。
-*/
+//链表是否有环
+var_dump($obj->isRing($n1));

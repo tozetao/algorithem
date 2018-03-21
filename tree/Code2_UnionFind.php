9deefd81dea933f0dd22824aba29e314eb9635cf
zetao <tozetao@163.com>
2018-03-21 08:21:50 +0800

    update

--- tree/Code2_UnionFind.php
+++ tree/Code2_UnionFind.php
@@ -21,28 +21,58 @@ class Node
  */
 class UnionFind
 {
-    public function isSameSet()
-    {
-
-    }
-
+    //寻找一个集合的代表节点，并将节点链打平
     public function findFather($node)
     {
         $parent = $node->parent;
-        if($parent == $node)
-            return $parent;
 
-        $symbol = $this->findFather($parent);
-        $node->parent = $symbol;
-        return $symbol;
+        if($parent != $node)
+            $parent = $this->findFather($parent);
+
+        $node->parent = $parent;
+        return $parent;
+    }
+
+    //是否相同集合
+    public function isSameSet($nodeA, $nodeB)
+    {
+        $result = $this->findFather($nodeA) === $this->findFather($nodeB);
+        return $result;
+    }
+
+    //将俩个集合合并成一个集合
+    public function union($nodeA, $nodeB)
+    {
+        $symoblA = $this->findFather($nodeA);
+        $symoblB = $this->findFather($nodeB);
+
+        if($symoblA !== $symoblB)
+        {
+            if($symoblA->count <= $symoblB->count)
+            {
+                $symoblA->parent = $symoblB;
+                $symoblB->count += $symoblA->count;
+            }else
+            {
+                $symoblB->parent = $symoblA;
+                $symoblA->count += $symoblB->count;
+            }
+        }
     }
 }
 
-$node = new Node(3);
-$node->parent = new Node(2);
-$node->parent->parent =  new Node(1);
+$node3 = new Node(3);
+$node2 = new Node(2);
+$node1 = new Node(1);
 
 $unionFind = new UnionFind();
-$symobl = $unionFind->findFather($node);
+$unionFind->union($node1, $node2);
+$unionFind->union($node1, $node3);
+
 echo '<pre/>';
-var_dump($symobl);
+print_r($node1);
+print_r($node3);
+
+var_dump($unionFind->isSameSet($node1, $node2));
+var_dump($unionFind->isSameSet($node1, $node3));
+var_dump($unionFind->isSameSet($node3, $node2));
